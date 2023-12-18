c6682165a3d3608ea071841433c277f9dcdba169
Éverton Toffanetto <evertondgn@hotmail.com>
2023-12-18 01:44:26 -0300

    chore: config absolute paths

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,25 @@
 module.exports = api => {
   api.cache(true)
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extensions: [
+            '.js',
+            '.jsx',
+            '.ts',
+            '.tsx',
+            '.json',
+            '.android.js',
+            '.android.tsx',
+            '.ios.js',
+            '.ios.tsx'
+          ]
+        }
+      ]
+    ]
   }
 }
