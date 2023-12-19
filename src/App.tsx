329d8b9f3b6edcc983c5c00097f1f50a7ddafeef
Éverton Toffanetto <evertondgn@hotmail.com>
2023-12-19 01:22:54 -0300

    feat: config fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
 import { StatusBar } from 'expo-status-bar'
 import { registerRootComponent } from 'expo'
 import Routes from 'routes'
+import {
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_600SemiBold,
+  useFonts
+} from '@expo-google-fonts/poppins'
 
 const App = () => {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_600SemiBold
+  })
+
+  if (!fontsLoaded) return null
+
   return (
     <>
       <Routes />
