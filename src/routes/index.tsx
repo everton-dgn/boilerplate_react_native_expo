bbc42ca330c7bd4cff0ec1c2d991b6ed9aa6b51a
Éverton Toffanetto <evertondgn@hotmail.com>
2023-12-19 05:16:28 -0300

    feat: custom font and color of header

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,12 +2,25 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Home from 'pages/Home'
 import Example from 'pages/Example'
+import T from 'theme'
 
 const { Navigator, Screen } = createNativeStackNavigator()
 
 const Routes = () => (
   <NavigationContainer>
-    <Navigator initialRouteName="home">
+    <Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerTitleStyle: {
+          fontFamily: T.fonts.familiesWeights.medium,
+          color: T.colors.dark
+        },
+        headerBackTitleStyle: {
+          fontFamily: T.fonts.familiesWeights.regular
+        },
+        headerTintColor: T.colors.dark
+      }}
+    >
       <Screen name="Home" component={Home} />
       <Screen name="Example" component={Example} />
     </Navigator>
