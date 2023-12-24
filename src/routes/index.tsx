ec39f292e03c998898a3f0040be50bce1663a8ff
Éverton Toffanetto <evertondgn@hotmail.com>
2023-12-23 22:12:51 -0300

    refactor: separated styles in route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,27 +1,31 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
-import { Home, Example } from 'pages'
+import { Example, Home } from 'pages'
 
 import T from 'theme'
+
+const screenOptionsStyles = {
+  headerStyle: {
+    backgroundColor: T.colors.blue
+  },
+  headerTitleStyle: {
+    fontFamily: T.fonts.familiesWeights.medium,
+    color: T.colors.white
+  },
+  headerBackTitleStyle: {
+    fontFamily: T.fonts.familiesWeights.regular
+  },
+  headerTintColor: T.colors.white,
+  headerTitleAlign: 'center',
+  headerBackTitleVisible: false
+} as const
 
 const { Navigator, Screen } = createNativeStackNavigator()
 
 const Routes = () => (
   <NavigationContainer>
-    <Navigator
-      initialRouteName="Home"
-      screenOptions={{
-        headerTitleStyle: {
-          fontFamily: T.fonts.familiesWeights.medium,
-          color: T.colors.dark
-        },
-        headerBackTitleStyle: {
-          fontFamily: T.fonts.familiesWeights.regular
-        },
-        headerTintColor: T.colors.dark
-      }}
-    >
+    <Navigator initialRouteName="Home" screenOptions={screenOptionsStyles}>
       <Screen name="Home" component={Home} />
       <Screen name="Example" component={Example} />
     </Navigator>
